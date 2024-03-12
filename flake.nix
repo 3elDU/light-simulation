5244d7d6829a38b0278b7186a2f61a803377a0f3
Zakhar Voloshchuk (3elDU) <3elduu@gmail.com>
2024-03-12 13:46:04 +0200

    feat(flake.nix): append library paths so that the binary would run correctly

--- flake.nix
+++ flake.nix
@@ -25,6 +25,12 @@
           pkg-config
           rust-bin.stable.latest.default
         ];
+
+        LD_LIBRARY_PATH = builtins.concatStringsSep ":" [
+          "${pkgs.xorg.libX11}/lib"
+          "${pkgs.xorg.libXi}/lib"
+          "${pkgs.libGL}/lib"
+        ];
       };
     }
   );
