cfe5fffd63fbe3299e32ebdfba5f799b1b5431c2
Zakhar Voloshchuk <zakhar.voloshcuk@a-team.global>
2026-02-11 19:09:05 +0100

    1/n - migrate from vanilla js to svelte

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,15 +1,9 @@
+import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-  root: "./frontend",
-  build: {
-    modulePreload: false,
-    outDir: "../dist",
-    emptyOutDir: true,
-    assetsDir: "assets",
-  },
-  plugins: [wasm()],
+  plugins: [wasm(), sveltekit()],
   worker: {
     format: "es",
     plugins() {
